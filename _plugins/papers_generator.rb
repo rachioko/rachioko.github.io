25e3b377143d7f977e991533cdf8b81cf8ee9d67
Rachi <47721192+rachioko@users.noreply.github.com>
2020-07-12 11:32:24 +0100

    Create papers_generator.rb

---
+++ _plugins/papers_generator.rb
@@ -0,0 +1,31 @@
+module Jekyll
+
+  class PaperPage < Page
+    def initialize(site, base, dir, papers)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'papers_index.html')
+      self.data['papers'] = papers
+      self.data['title'] = papers
+    end
+  end
+  
+  class PaperPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.layouts.key? 'papers_index'
+        dir = site.config['papers_dir'] || 'papers'
+        site.papers.each_key do |papers|
+          papers_dir = File.join(dir, Utils.slugify(papers))
+          site.pages << PaperPage.new(site, site.source, papers_dir, papers)
+        end
+      end
+    end
+  end
+
+end
